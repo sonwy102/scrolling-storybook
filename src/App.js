ddaee62a81ad72eafc8727d90cbeed72e137ca57
Wooyang Son <sonwy102@gmail.com>
2021-01-18 01:00:52 -0500

    add mapdata

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
+import React from "react";
 import './App.css';
-import book from './imgs/book.jpg'
-
-import Jumbotron from 'react-bootstrap/Jumbotron'
+import MapChart from './components/mapChart'
 
 function App() {
+  const [mapData, setMapData] = React.useState({
+    type: 'loading',
+    objects: 'loading',
+    arcs: 'loading',
+    bbox: 'loading',
+    transform: 'loading'
+  });
+
+  console.log('mapData: ', mapData);
+
+  const fetchJson = () => {
+    fetch("./data/world-happiness-dataset/countries-50m.json",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("data from fetchJson: ", data);
+      setMapData(data);
+    })
+    .catch(function(err) {
+      console.log(err, ' error')});
+  };
+
+  React.useEffect(() => {
+    fetchJson();
+  }, []);
+
   return (
     <div className="App">
-      <Jumbotron>
-        <img id="book" src={book} alt="book"></img>
-      </Jumbotron>
+      <MapChart mapData={mapData}></MapChart>
     </div>
   );
 }
